ace08774513cfd1a82828cd56668f4ce59014aab
rpharr <rpharr@richmond.edu>
2020-04-07 23:55:55 -0400

    more congig for heroku

--- server.js
+++ server.js
@@ -16,18 +16,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+
+app.use(routes);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+
 }
 
 // Send every request to the React app
 
-app.use(routes)
-  // Define any API routes before this runs
-  // app.get("*", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  // });
+
+// Define any API routes before this runs
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
