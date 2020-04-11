85661f973081442582784ecdcac8fe83882115be
rpharr <rpharr@richmond.edu>
2020-04-10 23:59:14 -0400

    styled paginator componenr

--- client/src/components/Paginator/index.js
+++ client/src/components/Paginator/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function Paginator(props){
   return(
-    <div style={props.searchResults === undefined ? {display: "none"} : {display: "block"}}>
+    <div className="paginator" style={props.searchResults === undefined ? {display: "none"} : {display: "block"}}>
       <button className="btn" id="prev" onClick={(event)=>props.handlePagination(event)}>Prev</button>
       <span>{(props.start+1)} - {props.start+props.numItems} of ({props.totalItems})</span>
       <button className="btn" id="next" onClick={(event)=>props.handlePagination(event)}>Next</button>
